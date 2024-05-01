1844eadb5075c9d82ed740ce35b42eb0a2166ea9
ObsoKnotty <taykirk1912@gmail.com>
2024-05-01 20:52:07 +0100

    Week 2 course work mid Week

---
+++ Course Work/W02/Active Learning notes W02.py
@@ -0,0 +1,22 @@
+# #      ACTIVITY 1
+
+# # Working with Numbers
+# pi = 3.14
+# print(pi)
+# #can do math operations in a print
+# firstNum = 9
+# secondNum = 2
+# print(firstNum + secondNum)
+# #this can be done with any operation /*-+
+# print(firstNum ** secondNum) #a double ** means exponent like 2 to the power of
+
+# # you cant print more than one type of variable
+# print(28 + 'days in february')# THIS WONT WORK
+
+#Type conversion
+# str() converts the var inside to a string
+print(str(28) + ' days in feb')
+# int() converts the var inside to a whole number, can only use whole numbers
+print(int('29'))
+# float() converts the var inside to a number that can have decimals
+print(int('2.9'))
---
+++ Course Work/W02/TeamActivity.py
@@ -0,0 +1,31 @@
+import math
+meter = 10000
+
+square_length = input('What is the length of the square in centimeters? ')
+square_num = ([int(i) for i in square_length.split() if i.isdigit()])
+square_area = (float(square_num[0])**2)
+
+if square_area >= meter:
+    print(f'The area of the square is: {square_area / meter} square meters')
+else:
+    print(f'The area of the square is: {square_area} square centimeters')
+
+rectangle_length = input('What is the length of the rectangle in centimeters? ')
+rectangle_width = input('What is the width of the rectangle in centimeters? ')
+rectangle_length_num = ([int(i) for i in rectangle_length.split() if i.isdigit()])
+rectangle_width_num = ([int(i) for i in rectangle_width.split() if i.isdigit()])
+rectangle_area = (float(rectangle_length_num[0]) * float(rectangle_width_num[0]))
+
+if rectangle_area >= meter:
+    print(f'The area of the rectangle is: {rectangle_area / meter} square meters')
+else:
+    print(f'The area of the rectangle is: {rectangle_area} square centimeters')
+
+circle_radius = input('What is the radius of the circle in centimeters? ')
+circle_radius_num = ([int(i) for i in circle_radius.split() if i.isdigit()])
+circle_area = (math.pi * float(circle_radius_num[0])**2)
+
+if circle_area >= meter:
+    print(f'The area of the circle is : {circle_area / meter} square meters')
+else:
+    print(f'The area of the circle is : {circle_area} square centimeters')
